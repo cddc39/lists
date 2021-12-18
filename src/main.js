9b650f1ae6633da8cdfc15e91559d8cd1e1a5eb2
David Mikalova <324548+dmikalova@users.noreply.github.com>
2021-12-17 18:26:42 -0800

    Revert "Added PWA"
    This reverts commit 3ce3b1b614178d8f8a240eb00c404c98bae9058b.

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import "@quasar/extras/material-icons/material-icons.css";
 import "quasar/src/css/index.sass";
 
 import App from "./App.vue";
-import './registerServiceWorker'
 
 createApp(App)
   .use(createPinia())
