3ce3b1b614178d8f8a240eb00c404c98bae9058b
David Mikalova <324548+dmikalova@users.noreply.github.com>
2021-12-17 18:22:33 -0800

    Added PWA

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import "@quasar/extras/material-icons/material-icons.css";
 import "quasar/src/css/index.sass";
 
 import App from "./App.vue";
+import './registerServiceWorker'
 
 createApp(App)
   .use(createPinia())
